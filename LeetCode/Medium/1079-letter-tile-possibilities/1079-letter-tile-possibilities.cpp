ba9eacb8441892084cad05ac8f91dca3574ba455
Shashwat Kumar <kshashwat.iit@gmail.com>
2026-07-29 10:25:12 +0530

    Hash Table Medium

--- LeetCode/Medium/1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
+++ LeetCode/Medium/1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -1,27 +1,23 @@
 class Solution {
-    int ans = 0;
     map<char, int> freq;
 
-    void rec(int lvl, string tiles) {
+    int rec(int lvl, string tiles) {
         int n = tiles.length();
-        ans++;
-        if (lvl == n) {
-            return;
-        }
+        int ret = 0;
         for (auto [c, f] : freq) {
             if (f > 0) {
                 freq[c]--;
-                rec(lvl + 1, tiles);
+                ret += rec(lvl + 1, tiles) + 1;
                 freq[c]++;
             }
         }
+        return ret;
     }
 
 public:
     int numTilePossibilities(string tiles) {
         for (auto& c : tiles)
             freq[c]++;
-        rec(0, tiles);
-        return ans - 1;
+        return rec(0, tiles);
     }
 };
