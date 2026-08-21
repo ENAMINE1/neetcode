143e2a1bf2df5459393cdd30fca7dad80df5ee5b
Shashwat Kumar <kshashwat.iit@gmail.com>
2026-08-21 13:09:06 +0530

    Depth-First Search Hard

--- LeetCode/Hard/2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ LeetCode/Hard/2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -2,24 +2,17 @@ class Solution {
     int ans = 0;
 
     vector<int> vis;
-    vector<int> par;
+    vector<int> depth;
 
-    void dfs(int node, int parent, vector<vector<int>>& g) {
+    void dfs(int node, int d, vector<vector<int>>& g) {
         vis[node] = 2;
-        par[node] = parent;
+        depth[node] = d;
 
         for (auto nn : g[node]) {
             if (vis[nn] == 1) {
-                dfs(nn, node, g);
+                dfs(nn, d + 1, g);
             } else if (vis[nn] == 2) {
-                int temp = node;
-                int cnt = 0;
-                while (nn != temp) {
-                    cnt++;
-                    temp = par[temp];
-                }
-                cnt++;
-                ans = max(ans, cnt);
+                ans = max(ans, depth[node] - depth[nn] + 1);
             }
         }
         vis[node] = 3;
@@ -30,7 +23,7 @@ public:
         int n = edges.size();
         vector<vector<int>> g(n);
         vis.assign(n, 1);
-        par.assign(n, -1);
+        depth.assign(n, 0);
 
         for(int i = 0; i < n; i++)
         {
@@ -41,7 +34,7 @@ public:
         for(int i = 0; i < n; i++)
         {
             if(vis[i] == 1)
-                dfs(i, -1, g);
+                dfs(i, 0, g);
         }
 
         return (ans == 0? -1: ans);
