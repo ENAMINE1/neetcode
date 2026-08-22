b45cb04d3fba4236e40ba583f0e087860a01d42c
Shashwat Kumar <kshashwat.iit@gmail.com>
2026-08-22 13:16:48 +0530

    Array Hard

--- LeetCode/Hard/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ LeetCode/Hard/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -5,23 +5,18 @@ public:
         int m = g[0].size();
         int xdir[4] = {0,0,1,-1};
         int ydir[4] = {1,-1,0,0};
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<>>pq;
+        deque<pair<int,int>>dq;
         vector<vector<int>>cost(n,vector<int>(m,1e9));
 
         cost[0][0] = 0;
-        pq.push({0,{0,0}});
+        dq.push_front({0,0});
 
-        while(!pq.empty()){
-            int c = pq.top().first;
-            auto node = pq.top().second;
-            pq.pop();
+        while(!dq.empty()){
+            auto node = dq.front();
+            dq.pop_front();
 
             int x = node.first;
             int y = node.second;
-
-            if(c > cost[x][y]){
-                continue;
-            }
 
             for(int i=0; i<4; i++){
                 int nx = x + xdir[i];
@@ -31,11 +26,12 @@ public:
                     continue;
                 }
 
-                int nc = c + (g[x][y] == i+1 ? 0 : 1);  // add +1 cost for other direction
+                int nc = (g[x][y] == i+1 ? 0 : 1);  // add +1 cost for other direction
 
-                if(cost[nx][ny] > nc){
-                    cost[nx][ny] = nc;
-                    pq.push({nc,{nx,ny}});
+                if(cost[nx][ny] > cost[x][y] + nc){
+                    cost[nx][ny] = cost[x][y] + nc;
+                    if(nc == 0) dq.push_front({nx,ny});
+                    else dq.push_back({nx,ny});
                 }
             }
             
