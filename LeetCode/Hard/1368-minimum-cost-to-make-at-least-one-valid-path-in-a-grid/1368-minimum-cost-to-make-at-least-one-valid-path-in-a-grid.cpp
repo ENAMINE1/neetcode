3f2639ca80a2d44dcbff33fe3d3d29143743b808
Shashwat Kumar <kshashwat.iit@gmail.com>
2026-08-24 08:36:47 +0530

    Array Hard

--- LeetCode/Hard/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ LeetCode/Hard/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,42 +1,45 @@
 class Solution {
+
+    vector<int> dx = {0, 0, 1, -1};
+    vector<int> dy = {1, -1, 0, 0};
+
 public:
-    int minCost(vector<vector<int>>& g) {
-        int n = g.size();
-        int m = g[0].size();
-        int xdir[4] = {0,0,1,-1};
-        int ydir[4] = {1,-1,0,0};
-        deque<pair<int,int>>dq;
-        vector<vector<int>>cost(n,vector<int>(m,1e9));
+    int minCost(vector<vector<int>>& grid) {
+        // construct the graph
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> dist(n, vector<int>(m, 1e9));
 
-        cost[0][0] = 0;
-        dq.push_front({0,0});
+        pair<int, int> start = {0, 0};
 
-        while(!dq.empty()){
+        deque<pair<int,int>> dq;
+        dq.push_front(start);
+        dist[0][0] = 0;
+        while(!dq.empty())
+        {
             auto node = dq.front();
             dq.pop_front();
 
-            int x = node.first;
-            int y = node.second;
-
-            for(int i=0; i<4; i++){
-                int nx = x + xdir[i];
-                int ny = y + ydir[i];
-
-                if(nx<0 || nx>=n || ny<0 || ny>=m){
+            for(int i = 0; i < 4; i++)
+            {
+                int nx = node.first + dx[i];
+                int ny = node.second + dy[i];
+                
+                if(nx >= n || nx < 0 || ny >= m || ny < 0)
                     continue;
-                }
+                
+                int cost = (i + 1 != grid[node.first][node.second]);
+                if(dist[nx][ny] > dist[node.first][node.second] + cost)
+                {
+                    if(cost)
+                        dq.push_back({nx, ny});
+                    else
+                        dq.push_front({nx, ny});
 
-                int nc = (g[x][y] == i+1 ? 0 : 1);  // add +1 cost for other direction
-
-                if(cost[nx][ny] > cost[x][y] + nc){
-                    cost[nx][ny] = cost[x][y] + nc;
-                    if(nc == 0) dq.push_front({nx,ny});
-                    else dq.push_back({nx,ny});
+                    dist[nx][ny] = dist[node.first][node.second] + cost;
                 }
             }
-            
-        }
-
-        return cost[n-1][m-1];
+        }  
+        return dist[n - 1][m - 1];    
     }
 };
