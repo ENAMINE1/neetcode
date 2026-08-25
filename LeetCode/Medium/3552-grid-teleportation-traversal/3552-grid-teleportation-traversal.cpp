3449dcefced8a2d1b46107ec39bcc556b296bc70
Shashwat Kumar <kshashwat.iit@gmail.com>
2026-08-25 09:04:35 +0530

    Array Medium

--- LeetCode/Medium/3552-grid-teleportation-traversal/3552-grid-teleportation-traversal.cpp
+++ LeetCode/Medium/3552-grid-teleportation-traversal/3552-grid-teleportation-traversal.cpp
@@ -1,99 +1,56 @@
 class Solution {
+
+    vector<int> dx = {0, 1, 0, -1};
+    vector<int> dy = {1, 0, -1, 0};
+
 public:
-    int minMoves(vector<string>& matrix) {
+    int minMoves(vector<string>& v) {
+        int n = v.size();
+        int m = v[0].size();
 
-        int m = matrix.size();
-        int n = matrix[0].size();
-
-        const int INF = 1e9;
-
-        vector<vector<int>> dist(
-            m, vector<int>(n, INF)
-        );
-
-        // Store positions of each portal.
-        vector<vector<pair<int, int>>> pos(26);
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] >= 'A' &&
-                    matrix[i][j] <= 'Z') {
-
-                    pos[matrix[i][j] - 'A'].push_back({i, j});
-                }
+        deque<pair<int, int>> dq;
+        vector<vector<int>> dist(n, vector<int>(m, 1e9));
+        dist[0][0] = 0;
+        dq.push_back({0, 0});
+        vector<int> vis(26, 0);
+        // mark the positions of all the alphabets
+        vector<vector<pair<int, int>>> idx_v(26);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (v[i][j] >= 'A' && v[i][j] <= 'Z')
+                    idx_v[v[i][j] - 'A'].push_back({i, j});
             }
         }
 
-        // Whether we have already expanded
-        // the teleport edges for this letter.
-        vector<bool> used(26, false);
-
-        deque<pair<int, int>> dq;
-
-        dist[0][0] = 0;
-        dq.push_front({0, 0});
-
-        int dx[] = {1, -1, 0, 0};
-        int dy[] = {0, 0, 1, -1};
-
         while (!dq.empty()) {
-
             auto [x, y] = dq.front();
             dq.pop_front();
 
-            int d = dist[x][y];
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
 
-            // ------------------------------------------------
-            // 0-cost teleport
-            // ------------------------------------------------
-            if (matrix[x][y] >= 'A' &&
-                matrix[x][y] <= 'Z') {
-
-                int c = matrix[x][y] - 'A';
-
-                if (!used[c]) {
-
-                    used[c] = true;
-
-                    for (auto [nx, ny] : pos[c]) {
-
-                        if (dist[nx][ny] > d) {
-
-                            dist[nx][ny] = d;
-
-                            // Cost = 0
-                            dq.push_front({nx, ny});
-                        }
+                if (nx < n && ny < m && nx >= 0 && ny >= 0 &&
+                    v[nx][ny] != '#') {
+                    if (dist[nx][ny] > dist[x][y] + 1) {
+                        dist[nx][ny] = dist[x][y] + 1;
+                        dq.push_back({nx, ny});
                     }
                 }
             }
 
-            // ------------------------------------------------
-            // 1-cost normal movement
-            // ------------------------------------------------
-            for (int k = 0; k < 4; k++) {
-
-                int nx = x + dx[k];
-                int ny = y + dy[k];
-
-                if (nx < 0 || nx >= m ||
-                    ny < 0 || ny >= n ||
-                    matrix[nx][ny] == '#') {
-                    continue;
-                }
-
-                if (dist[nx][ny] > d + 1) {
-
-                    dist[nx][ny] = d + 1;
-
-                    // Cost = 1
-                    dq.push_back({nx, ny});
+            if(v[x][y] != '.' && !vis[v[x][y] - 'A'])
+            {
+                vis[v[x][y] - 'A'] = 1;
+                for (auto [nx, ny] : idx_v[v[x][y] - 'A']) {
+                    if(dist[nx][ny] > dist[x][y]) {
+                        dist[nx][ny] = dist[x][y];
+                        dq.push_front({nx, ny});
+                    }
                 }
             }
         }
 
-        return dist[m - 1][n - 1] == INF
-                   ? -1
-                   : dist[m - 1][n - 1];
+        return (dist[n - 1][m - 1] == 1e9)?-1: dist[n -1][m - 1];
     }
 };
