04351702201ca6512b6e260daaf58760e922a074
Shashwat Kumar <kshashwat.iit@gmail.com>
2026-09-15 13:49:26 +0530

    String Medium

--- LeetCode/Medium/0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ LeetCode/Medium/0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,37 +1,39 @@
 class Solution {
     vector<vector<string>> ans;
 
-    bool isPalindrome(string s)
-    {
-        int n = s.length();
-        for(int i = 0; i < n / 2; i++){
-            if(s[i] != s[n - i - 1])
-                return false;
-        }
-        return true;
-    }     
+    int isPalindrome[20][20];
 
-    void rec(int idx, string &s, vector<string> &arr){
+    void rec(int idx, string& s, vector<string>& arr) {
         int n = s.length();
-        if(idx == n){
+        if (idx == n) {
             ans.push_back(arr);
             return;
         }
 
-        for(int i = idx + 1; i <= n; i++)
-        {
+        for (int i = idx + 1; i <= n; i++) {
             string left = s.substr(idx, i - idx);
-            if(isPalindrome(left))
-            {
+            if (isPalindrome[idx][i - 1]) {
                 arr.push_back(left);
                 rec(i, s, arr);
                 arr.pop_back();
             }
         }
     }
+
 public:
     vector<vector<string>> partition(string s) {
         vector<string> arr;
+        int n = s.length();
+        for (int len = 1; len <= n; len++) {
+            for (int i = 0; i + len - 1 < n; i++) {
+                int j = i + len - 1;
+                if (len <= 2)
+                    isPalindrome[i][j] = (len == 1 || s[i] == s[j]);
+                else
+                    isPalindrome[i][j] =
+                        (s[i] == s[j] && isPalindrome[i + 1][j - 1]);
+            }
+        }
         rec(0, s, arr);
         return ans;
     }
